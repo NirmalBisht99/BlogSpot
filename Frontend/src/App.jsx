600062229611ce6aa641d1edee32f63e4295db8c
NirmalBisht99 <nirmalbisht2599@gmail.com>
2026-02-25 01:53:10 +0530

    Fix case-sensitive import for Render deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import 'quill/dist/quill.snow.css'
 import { Route, Routes } from 'react-router-dom'
 import  Home  from './pages/Home'
 import  Blog  from './pages/Blog'
-import Layout from './pages/admin/layout'
+import Layout from './pages/admin/Layout'
 import Dashboard from './pages/admin/Dashboard'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
@@ -20,13 +20,13 @@ const App = () => {
       <Toaster />
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/log/:id' element={<Blog/>} />
     <Route path ='/admin' element={token ?<Layout/> : <Login/>}>
   <Route index element={<Dashboard/>}/>
 
-      <Route path='addBlog' element={<AddBlog/>}/>
-      <Route path='listBlog' element={<ListBlog/>}/>
-      <Route path='comments' element={<Comments/>}/>
+      <Route path='AddBlog' element={<AddBlog/>}/>
+      <Route path='ListBlog' element={<ListBlog/>}/>
+      <Route path='Comments' element={<Comments/>}/>
       </Route>
       </Routes>
     </div>
